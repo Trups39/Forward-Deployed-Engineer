5cb069bcb5bda8cb6ce973548b956893eed3d65a
TRUPTI <88398225+Trups39@users.noreply.github.com>
2024-12-01 21:14:51 -0500

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,13 +1,13 @@
 from strategies import BulkyPackageStrategy, HeavyPackageStrategy, PackageSorter
 
 def sort(width, height, length, mass):
-    sorter = PackageSorter(BulkyPackageStrategy())
+    sorter = PackageSorter(BulkyPackageStrategy())  #Check bulky
     if sorter.sort(width, height, length, mass):
-        sorter.set_strategy(HeavyPackageStrategy())  # Now check heavy after bulky
-        if sorter.sort(width, height, length, mass):  # Check both bulky and heavy
+        sorter.set_strategy(HeavyPackageStrategy())  #Check heavy after bulky
+        if sorter.sort(width, height, length, mass):  #Check both bulky and heavy
             return "REJECTED"
         return "SPECIAL"
     sorter.set_strategy(HeavyPackageStrategy())
-    if sorter.sort(width, height, length, mass):  # Check heavy for non-bulky packages
+    if sorter.sort(width, height, length, mass):  #Check heavy for non-bulky packages
         return "SPECIAL"
     return "STANDARD"
